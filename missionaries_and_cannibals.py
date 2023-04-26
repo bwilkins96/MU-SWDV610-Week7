aefb6b0576270ca673ad73d4021f5b82dd96b6f9
bwilkins96 <ben.jr.speed@gmail.com>
2023-04-26 19:41:33 -0400

    edge class

--- missionaries_and_cannibals.py
+++ missionaries_and_cannibals.py
@@ -11,3 +11,16 @@ class Vertex:
     def __hash__(self):
         return hash(id(self))
     
+class Edge:
+    def __init__(self, origin, dest, ele=None):
+        self._origin = origin
+        self._destination = dest
+        self._element = ele
+    
+    def endpoints(self):
+        return (self._origin, self._destination)
+    
+    def opposite(self, origin):
+        if origin is self._origin:
+            return self._destination
+        return self._origin
