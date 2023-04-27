b0779d212e6712e31ef6ae8725e55513aeae4ad5
bwilkins96 <ben.jr.speed@gmail.com>
2023-04-27 15:49:14 -0400

    testing and methods

--- graph.py
+++ graph.py
@@ -11,6 +11,9 @@ class Graph:
         
         def __hash__(self):
             return hash(id(self))
+        
+        def __repr__(self):
+            return '< ' + str(self.element()) + ' >'
     
     class Edge:
         def __init__(self, origin, dest, ele=None):
@@ -25,6 +28,9 @@ class Graph:
             if origin is self._origin:
                 return self._destination
             return self._origin
+        
+        def __repr__(self):
+            return '(' + str(self._origin) + ' --> ' + str(self._destination) + ')'
     
     def __init__(self, directed=False):
         self._outgoing = {}
@@ -38,6 +44,13 @@ class Graph:
     
     def vertices(self):
         return self._outgoing.keys()
+    
+    def get_vertex(self, ele):
+        for vert in self._outgoing:
+            if vert.element() == ele:
+                return vert
+        
+        return None
     
     def edge_count(self):
         total = 0
@@ -72,15 +85,17 @@ class Graph:
         if self.is_directed(): self._incoming[vert] = {}
         self._outgoing[vert] = {}
 
+        return vert
+
     def insert_edge(self, origin, dest, ele=None):
         edge = self.Edge(origin, dest, ele)
 
         self._outgoing[origin][dest] = edge
         self._incoming[dest][origin] = edge
 
-    def BFS(self, start_vert):
-        visited = {}
+        return edge
 
+    def BFS(self, start_vert, visited):
         level = [start_vert]
         while len(level) > 0:
             next_level = []
@@ -94,4 +109,53 @@ class Graph:
                         next_level.append(other_vert)
             
             level = next_level
+
+    def BFS2(self, start_vert, visited=None):
+        if visited == None:
+            visited = {start_vert: None}
+
+        level = [start_vert]
+        while len(level) > 0:
+            next_level = []
+
+            for vert in level:
+                yield vert
+                
+                for edge in self.incident_edges(vert):
+                    other_vert = edge.opposite(vert)
+
+                    if other_vert not in visited:
+                        visited[other_vert] = edge
+                        next_level.append(other_vert)
+            
+            level = next_level
+
+
+if __name__ == '__main__':
+    test = Graph()
+
+    a = test.insert_vertex('a')
+    b = test.insert_vertex('b')
+    c = test.insert_vertex('c')
+    d = test.insert_vertex('d')
+    e = test.insert_vertex('e')
+    f = test.insert_vertex('f')
+    g = test.insert_vertex('g')
+    h = test.insert_vertex('h')
+    i = test.insert_vertex('i')
+
+    edge_points = [(a,b), (a,d), (a,e), (b,c), (b,e), (e,d),
+                   (d,g), (d,h), (c,f), (f,h), (g,h), (h,i)]
+    
+    for points in edge_points:
+        test.insert_edge(points[0], points[1])
+
+    visited = {a: None}
+    test.BFS(a, visited)
+    
+    #for vert in test.BFS2(a, visited):
+    #    print(vert.element())
+
+    print(visited)
+
 
