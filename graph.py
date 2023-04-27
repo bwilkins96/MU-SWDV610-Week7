74233a7b1bd309165c0d1a014cbf2322b7ac6c3d
bwilkins96 <ben.jr.speed@gmail.com>
2023-04-26 20:26:28 -0400

    graph methods

--- graph.py
+++ graph.py
@@ -29,3 +29,51 @@ class Graph:
     def __init__(self, directed=False):
         self._outgoing = {}
         self._incoming = {} if directed else self._outgoing
+
+    def is_directed(self):
+        return self._incoming is not self._outgoing
+    
+    def vertex_count(self):
+        return len(self._outgoing)
+    
+    def vertices(self):
+        return self._outgoing.keys()
+    
+    def edge_count(self):
+        total = 0
+        for v in self._outgoing:
+            total += len(self._outgoing[v])
+
+        return total if self.is_directed() else total // 2
+    
+    def edges(self):
+        result = set()
+        for map in self._outgoing.values():
+            result.update(map.values())
+
+        return result
+    
+    def get_edge(self, origin, dest):
+        return self._outgoing[origin].get(dest)
+    
+    def degree(self, vert, outgoing=True):
+        edges = self._outgoing if outgoing else self._incoming
+        return len(edges[vert])
+    
+    def incident_edges(self, vert, outgoing=True):
+        edges = self._outgoing if outgoing else self._incoming
+
+        for e in edges[vert].values():
+            yield e
+
+    def insert_vertex(self, ele):
+        vert = self.Vertex(ele)
+
+        if self.is_directed(): self._incoming[vert] = {}
+        self._outgoing[vert] = {}
+
+    def insert_edge(self, origin, dest, ele=None):
+        edge = self.Edge(origin, dest, ele)
+
+        self._outgoing[origin][dest] = edge
+        self._incoming[dest][origin] = edge
